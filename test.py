e2c55f305041bbe9661059561d2934e8dd18c796
Ritik Jain <ritikjain1272@gmail.com>
2020-12-10 02:22:11 +0530

    Added unittest for GET request

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import json
 from app import app
 
 POST_URL = 'http://127.0.0.1:5000/api/post'
+GET_URL = 'http://127.0.0.1:5000/api/get'
 
 
 class PostConfessionTest(unittest.TestCase):
@@ -19,3 +20,13 @@ class PostConfessionTest(unittest.TestCase):
         response = self.app.post(POST_URL, data=payload,
                                  content_type='application/json')
         self.assertEqual(201, response.status_code)
+
+
+class GetConfessionTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_successfull_confession(self):
+        response = self.app.get(GET_URL, content_type='application/json')
+        self.assertEqual(200, response.status_code)
